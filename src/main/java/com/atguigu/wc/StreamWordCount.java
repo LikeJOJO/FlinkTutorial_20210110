397b9f0b405af880ee02ce69621411435bda90ec
孟庆祥 <mengqingxiang@atguigu.cn>
2021-08-17 13:08:37 +0800

    Day 2 am: source

--- src/main/java/com/atguigu/wc/StreamWordCount.java
+++ src/main/java/com/atguigu/wc/StreamWordCount.java
@@ -14,12 +14,13 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception{
         // 1. 创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        env.setParallelism(1);
 
         // 2. 读取文件
-//        DataStreamSource<String> lineStream = env.readTextFile("input/words.txt");
+        DataStreamSource<String> lineStream = env.readTextFile("input/words.txt");
 
         // 读取文本流
-        DataStreamSource<String> lineStream = env.socketTextStream("hadoop102", 7777);
+//        DataStreamSource<String> lineStream = env.socketTextStream("hadoop102", 7777);
 
         // 3. 分词转换并分组统计
         SingleOutputStreamOperator<Tuple2<String, Long>> sum = lineStream.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
