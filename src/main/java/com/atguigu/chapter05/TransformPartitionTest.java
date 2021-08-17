4abf8d401eb479020269a3d26f561a2a359a276a
孟庆祥 <mengqingxiang@atguigu.cn>
2021-08-17 17:13:37 +0800

    Day 2: transform finished

---
+++ src/main/java/com/atguigu/chapter05/TransformPartitionTest.java
@@ -0,0 +1,33 @@
+package com.atguigu.chapter05;
+
+import org.apache.flink.api.common.functions.Partitioner;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author atguigu-mqx
+ */
+public class TransformPartitionTest {
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(26);
+
+        env.addSource(new ClickSource())
+//                .rebalance()
+                .partitionCustom(new Partitioner<String>() {
+                    @Override
+                    public int partition(String key, int numPartitions) {
+                        return (int)(key.charAt(1) - 'a');
+                    }
+                }, new KeySelector<Event, String>() {
+                    @Override
+                    public String getKey(Event value) throws Exception {
+                        return value.user;
+                    }
+                })
+                .print();
+
+        env.execute();
+    }
+}
