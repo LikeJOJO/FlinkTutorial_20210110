40e25d6c20afe647557dbd5a6d73f5c8e01f56c4
孟庆祥 <mengqingxiang@atguigu.cn>
2021-08-18 17:11:10 +0800

    Day 3: window

---
+++ src/main/java/com/atguigu/chapter06/WindowTest.java
@@ -0,0 +1,53 @@
+package com.atguigu.chapter06;
+
+import com.atguigu.chapter05.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.time.Duration;
+
+/**
+ * @author atguigu-mqx
+ */
+public class WindowTest {
+    public static void main(String[] args) throws Exception{
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.getConfig().setAutoWatermarkInterval(100);
+
+        SingleOutputStreamOperator<Event> stream = env.fromElements(new Event("Mary", "./home", 1000L),
+                new Event("Bob", "./cart", 2000L),
+                new Event("Alice", "./prod?id=100", 3000L),
+                new Event("Alice", "./prod?id=200", 3500L),
+                new Event("Bob", "./prod?id=2", 2500L),
+                new Event("Alice", "./prod?id=300", 3600L),
+                new Event("Bob", "./home", 3000L),
+                new Event("Bob", "./prod?id=1", 2300L),
+                new Event("Bob", "./prod?id=3", 3300L))
+                // 乱序流的watermark生成
+                .assignTimestampsAndWatermarks( WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event element, long recordTimestamp) {
+                                return element.timestamp;
+                            }
+                        })
+                );
+
+        stream.keyBy(data -> data.user)
+                .window(TumblingEventTimeWindows.of(Time.hours(1)))    // 滚动事件时间窗口
+//                .window(SlidingEventTimeWindows.of(Time.hours(1), Time.minutes(5)))    // 滑动事件时间窗口
+//                .window(EventTimeSessionWindows.withGap(Time.seconds(2)))    // 事件时间会话窗口
+//                .countWindow(10, 2)     // 滑动计数窗口
+        ;
+
+        env.execute();
+    }
+}
