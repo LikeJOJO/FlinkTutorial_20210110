e508c2b4eaac2e29f75e6164e136562cf591d792
孟庆祥 <mengqingxiang@atguigu.cn>
2021-08-18 13:29:40 +0800

    Day 3 am: sink test

--- src/main/java/com/atguigu/chapter05/TransformPartitionTest.java
+++ src/main/java/com/atguigu/chapter05/TransformPartitionTest.java
@@ -11,14 +11,14 @@ public class TransformPartitionTest {
     public static void main(String[] args) throws Exception {
         // 创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(26);
+        env.setParallelism(4);
 
         env.addSource(new ClickSource())
 //                .rebalance()
                 .partitionCustom(new Partitioner<String>() {
                     @Override
                     public int partition(String key, int numPartitions) {
-                        return (int)(key.charAt(1) - 'a');
+                        return (int)(key.charAt(1) - 'a') % 4;
                     }
                 }, new KeySelector<Event, String>() {
                     @Override
